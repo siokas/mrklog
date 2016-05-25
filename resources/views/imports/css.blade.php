d24a8b473b6875f4f5d08bd1a9d3ea69f6cb517b
Apostolos Siokas <apostolossiokas@gmail.com>
2016-05-25 14:41:13 +0300

    Change css imports

--- resources/views/imports/css.blade.php
+++ resources/views/imports/css.blade.php
@@ -1,27 +1,27 @@
 @if($page == 'edit' || $page=='create')
-    <link href="/css/todo.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/todo.css') }}" rel="stylesheet" type="text/css">
 @endif
 
 @if($page == 'article')
-	<link href="/css/themify-icons.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="/css/flexslider.min.css" rel="stylesheet" type="text/css" media="all"/>
-	<link href="/css/lightbox.min.css" rel="stylesheet" type="text/css" media="all"/>
-	<link href="/css/theme.css" rel="stylesheet" type="text/css" media="all"/>
-	<link href="/css/tags.css" rel="stylesheet" type="text/css" media="all"/>
+	<link href="{{ asset('css/themify-icons.css') }} " rel="stylesheet" type="text/css" media="all" />
+	<link href="{{ asset('css/flexslider.min.css') }} " rel="stylesheet" type="text/css" media="all"/>
+	<link href="{{ asset('css/lightbox.min.css') }}" rel="stylesheet" type="text/css" media="all"/>
+	<link href="{{ asset('css/theme.css') }}" rel="stylesheet" type="text/css" media="all"/>
+	<link href="{{ asset('css/tags.css') }}" rel="stylesheet" type="text/css" media="all"/>
 @endif
 
 @if($page == 'main')
-	<link href="/css/search.css" rel="stylesheet" type="text/css" media="all"/>
-	<link href="/css/tags.css" rel="stylesheet" type="text/css" media="all"/>
+	<link href="{{ asset('css/search.css') }}" rel="stylesheet" type="text/css" media="all"/>
+	<link href="{{ asset('css/tags.css') }}" rel="stylesheet" type="text/css" media="all"/>
 @endif
 
-<link href="/css/pe-icon-7-stroke.css" rel="stylesheet" type="text/css" media="all">
-<link href="/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="{{ asset('css/pe-icon-7-stroke.css') }}" rel="stylesheet" type="text/css" media="all">
+<link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
 
-<link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all"/>
-<link href="/css/bootstrap-markdown.min.css" rel="stylesheet" type="text/css" media="all">
+<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" media="all"/>
+<link href="{{ asset('css/bootstrap-markdown.min.css') }}" rel="stylesheet" type="text/css" media="all">
 
-<link href="/css/clean-blog.css" rel="stylesheet">
+<link href="{{ asset('css/clean-blog.css') }}" rel="stylesheet">
 
 <style>
 	@font-face{
