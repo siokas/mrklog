ed9f02837355d283bef91919dd66a11af6adfbd0
Apostolos Siokas <apostolossiokas@gmail.com>
2016-05-25 14:45:56 +0300

    Change js imports

--- resources/views/imports/js.blade.php
+++ resources/views/imports/js.blade.php
@@ -1,25 +1,25 @@
-<script src="/js/jquery.min.js"></script>
-<script src="/js/bootstrap.min.js"></script>
-<script src="/js/flexslider.min.js"></script>
-<script src="/js/lightbox.min.js"></script>
-<script src="/js/twitterfetcher.min.js"></script>
-<script src="/js/spectragram.min.js"></script>
-<script src="/js/smooth-scroll.min.js"></script>
-<script src="/js/jquery.plugin.min.js"></script>
-<script src="/js/placeholders.min.js"></script>
-<script src="/js/scripts.js"></script>
-<script src="/js/marked.min.js"></script>
-<script src="/js/to-markdown.js"></script>
-<script src="/js/moment.js"></script>
-<script src="/js/vue.js"></script>
-<script src="/js/vue-resource.js"></script>
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/flexslider.min.js') }}"></script>
+<script src="{{ asset('js/lightbox.min.js') }}"></script>
+<script src="{{ asset('js/twitterfetcher.min.js') }}"></script>
+<script src="{{ asset('js/spectragram.min.js') }}"></script>
+<script src="{{ asset('js/smooth-scroll.min.js') }}"></script>
+<script src="{{ asset('js/jquery.plugin.min.js') }}"></script>
+<script src="{{ asset('js/placeholders.min.js') }}"></script>
+<script src="{{ asset('js/scripts.js') }}"></script>
+<script src="{{ asset('js/marked.min.js') }}"></script>
+<script src="{{ asset('js/to-markdown.js') }}"></script>
+<script src="{{ asset('js/moment.js') }}"></script>
+<script src="{{ asset('js/vue.js') }}"></script>
+<script src="{{ asset('js/vue-resource.js') }}"></script>
 
 @if($page == 'edit' || $page == 'create')
-    <script src="/js/store.js"></script>
-    <script src="/js/app.js"></script>
-    <script src="/js/bootstrap-markdown.js"></script>
+    <script src="{{ asset('js/store.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/bootstrap-markdown.js') }}"></script>
 @endif
 
 @if($page == 'main')
-	<script src="/js/vue-main.js"></script>
+	<script src="{{ asset('js/vue-main.js') }}"></script>
 @endif
