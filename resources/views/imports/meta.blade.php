b6fd5f9c05a3ff8f8a69c89ba864893110c6a167
Apostolos Siokas <apostolossiokas@gmail.com>
2016-05-31 21:57:06 +0300

    meta chaged

--- resources/views/imports/meta.blade.php
+++ resources/views/imports/meta.blade.php
@@ -1,3 +1,4 @@
+
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta http-equiv="cache-control" content="private, max-age=0, no-cache">
@@ -5,25 +6,25 @@
 <meta http-equiv="expires" content="0">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!-- for Google -->
-<meta name="description" content="A blog that lets you write posts using markdown language without having account! It is the simplest and quickest blog you've ever imagine. Just click CREATE NEW POST and start writting!" />
-<meta name="keywords" content="markdown, blog, author, post, article, title, mrklog, markup, php, laravel, vuejs, javascript, bootstrap" />
+@if($page == "article")
+	<!-- for Google -->
+	<meta name="description" content="A blog that lets you write posts using markdown language without having account! It is the simplest and quickest blog you've ever imagine. Just click CREATE NEW POST and start writting!" />
+	<meta name="keywords" content="markdown, blog, author, post, article, title, mrklog, markup, php, laravel, vuejs, javascript, bootstrap" />
 
-<meta name="author" content="Apostolos Siokas" />
-<meta name="copyright" content="Apostolos Siokas" />
-<meta name="application-name" content="mrklog" />
+	<meta name="author" content="Apostolos Siokas" />
+	<meta name="copyright" content="Apostolos Siokas" />
+	<meta name="application-name" content="mrklog" />
 
-<!-- for Facebook -->          
-<meta property="og:title" content="Mrklog" />
-<meta property="og:type" content="article" />
-<meta property="og:image" content="http://mrklog.com/img/icon.png" />
-<meta property="og:url" content="http://mrklog.com" />
-<meta property="og:description" content="A blog that lets you write posts using markdown language without having account! It is the simplest and quickest blog you've ever imagine. Just click CREATE NEW POST and start writting!" />
+	<!-- for Facebook -->          
+	<meta property="og:title" content="Mrklog - {{ $post->title }}" />
+	<meta property="og:type" content="article" />
+	<meta property="og:image" content="http://mrklog.com/img/ena.jpg" />
+	<meta property="og:url" content="http://mrklog.com/posts/{{ $post->id }}" />
+	<meta property="og:description" content="A blog that lets you write posts using markdown language without having account! It is the simplest and quickest blog you've ever imagine. Just click CREATE NEW POST and start writting!" />
 
-<!-- for Twitter -->          
-<meta name="twitter:card" content="summary" />
-<meta name="twitter:title" content="mrklog" />
-<meta name="twitter:description" content="A blog that lets you write posts using markdown language without having account! It is the simplest and quickest blog you've ever imagine. Just click CREATE NEW POST and start writting! " />
-<meta name="twitter:image" content="http://mrklog.com/img/icon.png" />
-
-<link rel=“canonical”>
+	<!-- for Twitter -->          
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:title" content="mrklog" />
+	<meta name="twitter:description" content="A blog that lets you write posts using markdown language without having account! It is the simplest and quickest blog you've ever imagine. Just click CREATE NEW POST and start writting! " />
+	<meta name="twitter:image" content="http://mrklog.com/img/icon.png" />
+@endif
